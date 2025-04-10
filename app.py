f523c5b593b7594fb8b640ebfd36b3689feb9646
prytm <125776274+prytm@users.noreply.github.com>
2025-04-10 07:57:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,119 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy
+from scipy.spatial.distance import mahalanobis
 
 # Load data
-final_df = pd.read_csv('final_df (5).csv', delimiter=',')
+final_df = pd.read_csv('final_df.csv', delimiter=',')
+df_var = pd.read_csv('df_var_new.csv', delimiter = ',')
 
-# Judul Aplikasi
-st.title('Risk Projection')
+st.set_page_config(
+    page_title="Risk Projection",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-# Input pengguna untuk target
-st.header("Input Stock Target Data")
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-target_stock = st.text_input("Stock Target Code (ex: CBDK.JK):", value="CBDK.JK")
+# Custom CSS to inject into Streamlit
+st.markdown("""
+<style>
 
-# Menggunakan st.columns untuk mengatur layout
-col1, col2 = st.columns(2)  # Baris pertama: 2 kolom
-col3, col4 = st.columns(2)  # Baris kedua: 2 kolom
+/* Adjust the size and alignment of the CALL and PUT value containers */
+.metric-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    width: auto;
+    margin: 0 auto;
+}
 
-# Input di baris pertama
-with col1:
-    target_aset = st.number_input("Total Aset Target (in IDR):", value=18156999226)
-with col2:
-    target_eku = st.number_input("Total Ekuitas Target (in IDR):", value=8142575417)
+/* Custom classes for CALL and PUT values */
+.metric-99 {
+    background-color: #90ee90; /* Light green background */
+    color: black; /* Black font color */
+    margin-right: 10px; /* Spacing between CALL and PUT */
+    border-radius: 10px; /* Rounded corners */
+}
 
-# Input di baris kedua
-with col3:
-    target_mc = st.number_input("Market Cap Target (in IDR):", value=2301591670000)
-with col4:
-    target_laba = st.number_input("Laba Bersih Periode Tahun Berjalan (in IDR):", value=479102365)
+.metric-1 {
+    background-color: #ffcccb; /* Light red background */
+    color: black; /* Black font color */
+    border-radius: 10px; /* Rounded corners */
+}
 
-# Daftar pilihan subsektor
-subsektor_options = [
-    'Oil, Gas, & Coal', 'Basic Materials', 'Banks',
-    'Healthcare Equipment & Providers', 'Software & IT Service',
-    'Logistics & Deliveries', 'Food & Beverage', 'Industrial Goods',
-    'Consumer Services', 'Telecommunication', 'Industrial Services',
-    'Retailing', 'Automobiles & Components', 'Alternative Energy',
-    'Media & Entertainment', 'Properties & Real Estate',
-    'Heavy Constructions & Civil', 'Nondurable Household Products ',
-    'Leisure Goods', 'Household Goods', 'Utilities',
-    'Food & Staples Retailing ', 'Technology Hardware',
-    'Financing Service', 'Property & Real Estate',
-    'Phramaceuticals & Healthcare', 'Utilites',
-    'Multi Sector Holdings', 'Nondurable Household Products',
-    'Apparel & Luxury Goods'
-]
+/* Style for the value text */
+.metric-value {
+    font-size: 1.5rem; /* Adjust font size */
+    font-weight: bold;
+    text-align: center;
+    margin: 0; /* Remove default margins */
+}
 
-# Dropdown untuk memilih subsektor
-target_subsektor = st.selectbox("Sub Sektor Target:", options=subsektor_options, index=subsektor_options.index('Property & Real Estate'))
+/* Style for the label text */
+.metric-label {
+    font-size: 1rem; /* Adjust font size */
+    margin-bottom: 4px; /* Spacing between label and value */
+}
 
+</style>
+""", unsafe_allow_html=True)
+
+# Navbar di sebelah kiri
+with st.sidebar:
+    st.title("📊 Risk-Projection Model")
+    st.write("`Created by:`")
+    linkedin_url = "https://www.linkedin.com/in/prytm/"
+    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Priya Tammam`</a>', unsafe_allow_html=True)
+
+    target_stock = st.text_input("Stock Target Code (ex: KAQI.JK):", value = "KAQI.JK")
+    target_aset = st.number_input("Total Assets Target (in IDR):", value=74129409370)
+    target_eku = st.number_input("Total Equities Target (in IDR):", value=59158929209)
+    target_mc = st.number_input("Market Cap Target (in IDR):", value=531000000)
+    target_laba = st.number_input("net Profit Current Year Period (in IDR):", value=73414622023)
+
+    # Daftar pilihan subsektor
+    subsektor_options = [
+        'Oil, Gas, & Coal', 'Basic Materials', 'Banks',
+        'Healthcare Equipment & Providers', 'Software & IT Service',
+        'Logistics & Deliveries', 'Food & Beverage', 'Industrial Goods',
+        'Consumer Services', 'Telecommunication', 'Industrial Services',
+        'Retailing', 'Automobiles & Components', 'Alternative Energy',
+        'Media & Entertainment', 'Properties & Real Estate',
+        'Heavy Constructions & Civil', 'Nondurable Household Products ',
+        'Leisure Goods', 'Household Goods', 'Utilities',
+        'Food & Staples Retailing ', 'Technology Hardware',
+        'Financing Service', 'Property & Real Estate',
+        'Phramaceuticals & Healthcare', 'Utilites',
+        'Multi Sector Holdings', 'Nondurable Household Products',
+        'Apparel & Luxury Goods'
+    ]
+
+    # Dropdown untuk memilih subsektor
+    target_subsektor = st.selectbox("Sub Sektor Target:", options=subsektor_options, index=subsektor_options.index('Property & Real Estate'))
+
+    # Informasi tempat mendapatkan data
+    st.markdown("---")
+    st.markdown("Get the financial report IPO data from<br>**[e-IPO Indonesia](https://e-ipo.co.id/)**", unsafe_allow_html=True)
+    
 # Konversi ke DataFrame
 comparison_table = pd.DataFrame(final_df)
-
+    
 def calculate_percentage(filtered_table):
     """
     Menghitung persentase perbedaan dengan saham target.
@@ -76,29 +137,33 @@ def calculate_percentage(filtered_table):
     sorted_total = sorted(total_percentage.items(), key=lambda x: abs(x[1]))[:3]
 
     return sorted_total, percentage_details
-
+    
 def compare_with_subsektor():
     """
     Membandingkan dengan saham dalam subsektor yang sama.
     """
     filtered_table = comparison_table[(comparison_table['Sub Sektor'] == target_subsektor) &
                                       (comparison_table['Kode'] != target_stock)]
+    
     if filtered_table.empty:
-        st.warning(f"Warning: Tidak ada saham lain dalam subsektor {target_subsektor} untuk dibandingkan.\n")
-        return [], {}
-
-    return calculate_percentage(filtered_table)
-
+        filtered_table = comparison_table[ (comparison_table['Kode'] != target_stock)]
+    
+    return calculate_percentage(filtered_table, target_aset, target_mc, target_eku, target_laba)
+    
 def compare_without_subsektor():
     """
     Membandingkan dengan semua saham tanpa mempertimbangkan subsektor.
     """
     filtered_table = comparison_table[comparison_table['Kode'] != target_stock]
+    
     return calculate_percentage(filtered_table)
 
-# Jalankan perbandingan
 min_stocks_with_subsektor, details_with_subsektor = compare_with_subsektor()
 min_stocks_without_subsektor, details_without_subsektor = compare_without_subsektor()
+
+subsektor_stock = min_stocks_with_subsektor[0][0]
+subsektor_data = df_var[df_var['Kode'] == subsektor_stock]
+var = subsektor_data['VaR'].values[0]
 
 # Fungsi untuk membuat DataFrame dari hasil perbandingan
 def create_result_df(sorted_stocks, details):
@@ -109,16 +174,14 @@ def create_result_df(sorted_stocks, details):
     for stock, _ in sorted_stocks:
         row = {
             'Kode': stock,
-            'Total Aset': f"{details[stock]['Total Aset']:.2f}%",
-            'Total Ekuitas': f"{details[stock]['Total Ekuitas']:.2f}%",
-            'Market Cap': f"{details[stock]['Market Cap']:.2f}%",
-            'Laba Bersih': f"{details[stock]['Laba Bersih']:.2f}%",
+            'Percentage': details[stock],
         }
         data.append(row)
+        
     return pd.DataFrame(data)
 
 # Fungsi untuk menghitung Bollinger Bands
-def calculate_bollinger_bands(data, window=15):
+def calculate_bollinger_bands(data, window=10):
     """
     Menghitung Bollinger Bands.
     """
@@ -129,99 +192,79 @@ def calculate_bollinger_bands(data, window=15):
     return sma, upper_band, lower_band
 
 # Tampilkan hasil dengan subsektor jika ada
-st.write("Results considering Sub-Sector")
+st.title("Mahalanobis Risk-Projection Model")
+
+# Tabel Input
+input_data = {
+    "Stock" : [target_stock],
+    "Total Assets" : [target_aset],
+    "Market Cap" : [target_mc],
+    "Total Equities" : [target_eku],
+    "Net Income" : [target_laba]
+}
+input_df = pd.DataFrame(input_data)
+st.table(input_df)
+
+# VaR Value
+
+st.markdown(f"""
+    <div class = "metric-container metric-1">
+        <div>
+            <div class = "metric-label">📉 Absolute Value at Risk</div>
+            <div class = "metric-value">{var:.2f}</div>
+        </div>
+    </div>
+""", unsafe_allow_html = True)
+
+st.markdown("")
+st.markdown("")
+
+st.header("Weekly Returns Plot Projection")
+st.info("Explore how the daily returns projection with 95% confidence interval Moving Average fluctuate 3 months after IPO")
+
+subsektor_stock = min_stocks_with_subsektor[0][0]
+target_date_subsektor = final_df[final_df['Kode'] == subsektor_stock]['Date'].iloc[0]
+
+try:
+    data = yf.download(subsektor_stock, start=target_date_subsektor, end=pd.to_datetime(target_date_subsektor) + pd.DateOffset(months = 3), interval = '1d')['Close']
+    daily_returns_1 = ((data.shift(-4) - data) / data).dropna()
+
+    sma, upper_band, lower_band = calculate_bollinger_bands(daily_returns_1)
+
+    data = data.squeeze()
+    sma = sma.squeeze()
+    upper_band = upper_band.squeeze()
+    lower_band = lower_band.squeeze()
+
+    plt.style.use('dark_background')  # Gunakan style dark
+    
+    plt.figure(figsize=(12, 5))
+    plt.plot(daily_returns_1.index, daily_returns_1.values, label='Daily Return', color='cyan')
+    plt.plot(sma, label='SMA (10)', color='orange')
+    plt.plot(upper_band, label='Upper Band', linestyle='dashed', linewidth=1.1, color='green', alpha = 0.5)
+    plt.plot(lower_band, label='Lower Band', linestyle='dashed', linewidth=1.1, color='red', alpha = 0.5)
+    plt.fill_between(daily_returns_1.index, lower_band, upper_band, color='gray', alpha=0.2)
+    
+    plt.title(f"{subsektor_stock} Weekly Returns", color='white')
+    plt.xlabel("Date", color='white')
+    plt.ylabel("Weekly Returns", color='white')
+    plt.tick_params(colors='white')  # Warna sumbu x & y
+    plt.legend()
+    plt.grid(True, color='gray', alpha = 0.5)
+    st.pyplot(plt)
+except Exception as e:
+    st.error(f"Error fetching data for {subsektor_stock}: {e}")
+    st.write("Tidak ada hasil")
+
+st.header("Closest Stocks")
+
+st.write("Closest stocks considering Sub-Sector")
 if min_stocks_with_subsektor:
     df_with_subsektor = create_result_df(min_stocks_with_subsektor, details_with_subsektor)
-    col_df, col_plot = st.columns(2)
-
-    with col_df:
-        st.write('Closest Stock Considering Sub-Sector')
-        st.dataframe(df_with_subsektor, use_container_width=True)  # Perbaikan di sini
-
-    with col_plot:
-        subsektor_stock = min_stocks_with_subsektor[0][0]
-        target_date_subsektor = final_df[final_df['Kode'] == subsektor_stock]['Date'].iloc[0]
-
-        try:
-            st.write(f"Daily Returns Graph; {subsektor_stock}")
-            data = yf.download(subsektor_stock, start=target_date_subsektor, end=pd.to_datetime(target_date_subsektor) + pd.DateOffset(years=1))['Close']
-            daily_returns_1 = data.pct_change().dropna()
-
-            # Hitung Bollinger Bands
-            sma, upper_band, lower_band = calculate_bollinger_bands(daily_returns_1)
-
-            data = data.squeeze()
-            sma = sma.squeeze()
-            upper_band = upper_band.squeeze()
-            lower_band = lower_band.squeeze()
-
-            plt.figure(figsize=(12, 5)) # Ukuran plot disesuaikan
-            plt.plot(daily_returns_1.index, daily_returns_1.values, label='Daily Return')
-            plt.plot(sma, label='SMA (10)')
-            plt.plot(upper_band, label='Upper Band', linestyle='dashed', linewidth=1.1)
-            plt.plot(lower_band, label='Lower Band', linestyle='dashed', linewidth=1.1)
-            plt.fill_between(daily_returns_1.index, lower_band, upper_band, color='gray', alpha=0.2)
-            plt.title(f"{subsektor_stock} Daily Returns")
-            plt.xlabel("Date")
-            plt.ylabel("Daily Returns")
-            plt.legend()
-            plt.grid(True)
-            st.pyplot(plt)
-        except Exception as e:
-            st.error(f"Error fetching data for {subsektor_stock}: {e}")
-else:
-    st.write("Tidak ada hasil dalam subsektor yang sama.\n")
+    st.dataframe(df_with_subsektor, use_container_width=True)  # Perbaikan di sini
 
 # Bagian tanpa subsektor
-st.write("Results without considering Sub-Sector")
+st.write("Closest stocks without considering Sub-Sector")
 if min_stocks_without_subsektor:
     df_without_subsektor = create_result_df(min_stocks_without_subsektor, details_without_subsektor)
-
-    # Bagi layout menjadi dua kolom: DataFrame dan Plot
-    col_df, col_plot = st.columns(2)
-
-    with col_df:
-        st.write('Closest Stock without Considering Sub-Sector')
-        st.dataframe(df_without_subsektor, use_container_width=True)  # Perbaikan di sini
-
-    with col_plot:
-        not_subsektor_stock = min_stocks_without_subsektor[0][0]
-        target_date_not_subsektor = final_df[final_df['Kode'] == not_subsektor_stock]['Date'].iloc[0]
-
-        try:
-            st.write(f"Daily Returns Graph; {not_subsektor_stock}")
-            data = yf.download(not_subsektor_stock, start=target_date_not_subsektor, end=pd.to_datetime(target_date_not_subsektor) + pd.DateOffset(years=1))['Close']
-            daily_returns_2 = data.pct_change().dropna()
-
-            # Hitung Bollinger Bands
-            sma, upper_band, lower_band = calculate_bollinger_bands(daily_returns_2)
-            # Ubah menjadi 1D array
-            data = data.squeeze()
-            sma = sma.squeeze()
-            upper_band = upper_band.squeeze()
-            lower_band = lower_band.squeeze()
-
-            plt.figure(figsize=(12, 5)) # Ukuran plot disesuaikan
-            plt.plot(daily_returns_2.index, daily_returns_2.values, label='Daily Return')
-            plt.plot(sma, label='SMA (10)')
-            plt.plot(upper_band, label='Upper Band', linestyle='dashed', linewidth=1.1)
-            plt.plot(lower_band, label='Lower Band', linestyle='dashed', linewidth=1.1)
-            plt.fill_between(daily_returns_2.index, lower_band, upper_band, color='gray', alpha=0.2)
-            plt.title(f"{not_subsektor_stock} Daily Returns")
-            plt.xlabel("Date")
-            plt.ylabel("Daily Returns")
-            plt.legend()
-            plt.grid(True)
-            st.pyplot(plt)
-        except Exception as e:
-            st.error(f"Error fetching data for {not_subsektor_stock}: {e}")
-else:
-    st.write("Tidak ada hasil yang ditemukan.\n")
-
-st.header("Risk Projection")
-df_var = pd.read_csv('df_var_99.csv')
-subsektor_data = df_var[df_var['Kode'] == subsektor_stock]
-not_subsektor_data = df_var[df_var['Kode'] == not_subsektor_stock]
-
-result_df = pd.concat([subsektor_data, not_subsektor_data], axis=0)
-st.write(result_df)
+    st.dataframe(df_without_subsektor, use_container_width=True)  # Perbaikan di sini
